fc171b66638c3f3adb0942c3c1286aac08b6b784
lauanderson-rael <lauanderson38@gmail.com>
2024-01-16 01:19:19 -0300

    Content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,9 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCard:string = ""
+  //https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg
+  @Input()
+  cardTitle: string = ""
+  @Input()
+  id:string = "0"
 
-  constructor() { }
-
+  constructor() {}
+  
   ngOnInit(): void {
   }
 
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "NOVO HOMEM DE FERRO EM 3D ANUNCIADO",
+        "description": "Marvel studios anuncia novo filme do homem de ferro para 2024",
+        "photoCover":"https://e.rpp-noticias.io/xlarge/2023/12/04/361736_1509718.webp"   
+    },
+
+    {
+        "id":"2",
+        "title": "Marvel investe pesado no novo filme do volverine 2024",
+        "description": " Afinal, sabemos que Wolverine é mais centrado e prefere a solidão",
+        "photoCover":"https://cinepop.com.br/wp-content/uploads/2022/10/deadpool3.jpg"   
+    },
+
+    {
+        "id":"3",
+        "title": "Ordem completa dos filmes do Homem-Aranha",
+        "description": " Saiba mais sobre a ordem dos filmes do amado Homen-Aranha",
+        "photoCover":"https://s.aficionados.com.br/imagens/ordem-completa-dos-filmes-do-homem-aranha_f.jpg"   
+    },
+
+    {
+        "id":"4",
+        "title": "A genética por trás dos X-men",
+        "description": "O principal objetivo dos X-Men foi tentar ser uma história anti-preconceito, mostrar que o bem e o mal existem dentro da mesma pessoa",
+        "photoCover":"https://i0.wp.com/profissaobiotec.com.br/wp-content/uploads/2017/04/XMEN-FACTS-INTRO.jpg?fit=660%2C350&ssl=1"   
+    }
+    
+]
+    
+ 
